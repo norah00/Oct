1c22e2bbbda148ae90e5054cb743376b13988933
norah00 <112525238+norah00@users.noreply.github.com>
2022-10-25 21:01:11 -0400

    1

---
+++ js/my_jquery_demo_1.js
@@ -0,0 +1,8 @@
+
+$("#menu_1>.menu_btn").click(function(){
+    console.log("click"); 
+    $("#menu_1>.menu_list").toggleClass("show1");
+});
+$("#menu_2>.menu_btn").click(function(){
+    $("#menu_2 > .menu_list").toggleClass("show2");
+});
